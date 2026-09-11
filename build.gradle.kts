2b0b80be38fe7946814b89ce07d96d7329f216e7
Vyacheslav V. Verman <62892240+vvverman@users.noreply.github.com>
2026-09-11 12:02:33 +0300

    Register Android KMP plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     alias(libs.plugins.kotlinSerialization) apply false
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
+    alias(libs.plugins.androidKotlinMultiplatformLibrary) apply false
 }
