3114a4f1c8ed4f70df9f1d235a9584b62e8a99bc
Vyacheslav V. Verman <62892240+vvverman@users.noreply.github.com>
2026-09-11 23:21:03 +0300

    Подключить datetime в общий UI Kasha

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,6 +43,7 @@ kotlin {
             implementation(compose.components.resources)
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.serialization.json)
+            implementation(libs.kotlinx.datetime)
         }
         commonTest.dependencies {
             implementation(kotlin("test"))
