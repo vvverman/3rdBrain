fa9df60d2d04c51f837283d697a1fc83128ccf5a
Vyacheslav V. Verman <62892240+vvverman@users.noreply.github.com>
2026-09-12 23:55:26 +0300

    refactor: добавить общий AI catalog модуль

---
+++ aiCatalog/build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKotlinMultiplatformLibrary)
+}
+
+kotlin {
+    jvm()
+    jvmToolchain(21)
+
+    android {
+        namespace = "ru.vrmn.kasha.ai.catalog"
+        compileSdk = 37
+        minSdk = 26
+    }
+
+    iosArm64()
+    iosSimulatorArm64()
+
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs { browser() }
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(project(":kashaCore"))
+        }
+        commonTest.dependencies {
+            implementation(kotlin("test"))
+        }
+    }
+}
