718d21f60bc82702a93946fa64c4483e33e6230a
Vyacheslav V. Verman <62892240+vvverman@users.noreply.github.com>
2026-09-13 00:04:23 +0300

    fix: добавить serialization runtime в aiCatalog

--- aiCatalog/build.gradle.kts
+++ aiCatalog/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             implementation(project(":kashaCore"))
+            implementation(libs.kotlinx.serialization.json)
         }
         commonTest.dependencies {
             implementation(kotlin("test"))
